dc33c284ed0a37884dada7626f5d812cf6047754
Jhon <jhonatasimonetto@gmail.com>
2020-03-02 18:20:20 -0300

    FirstCommit projeto criado

---
+++ src/main/java/br/com/sen/senbe/SenBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.sen.senbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sen/senbe/SenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sen.senbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
