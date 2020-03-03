ab1bb8911d45e987775544eee7ab84083eec8ab9
Jhon <jhonatasimonetto@gmail.com>
2020-03-02 23:38:29 -0300

    Db configurado test jpa

--- src/main/java/br/com/sen/senbe/domain/Pedido.java
+++ src/main/java/br/com/sen/senbe/domain/Pedido.java
@@ -2,9 +2,17 @@ package br.com.sen.senbe.domain;
 
 import java.io.Serializable;
 
-public class Pedido implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String solicitante;
 	private String descricao;
