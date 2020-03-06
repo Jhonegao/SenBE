1c221152ae98d0bd711e70bf09e7a4005b1c616e
Jhon <jhonatasimonetto@gmail.com>
2020-03-06 07:15:22 -0300

    Cors confi

--- src/main/java/br/com/sen/senbe/services/PedidoService.java
+++ src/main/java/br/com/sen/senbe/services/PedidoService.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.com.sen.senbe.domain.Pedido;
 import br.com.sen.senbe.repositories.PedidoRepository;
 
+@CrossOrigin
 @Service
 public class PedidoService {
 
